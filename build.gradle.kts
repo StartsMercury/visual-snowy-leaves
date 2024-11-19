8c820e4155f8ea091e3ac67c2fb0a698cc607c8a
StartsMercury <89975834+StartsMercury@users.noreply.github.com>
2024-11-19 21:40:58 +0800

    Update to Minecraft 1.21.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ object Constants {
     const val VERSION = "0.4.0"
 
     const val VERSION_JAVA = 21
-    const val VERSION_MINECRAFT = "1.21.2"
+    const val VERSION_MINECRAFT = "1.21.3"
 }
 
 plugins {
@@ -57,12 +57,12 @@ dependencies {
     mappings(loom.officialMojangMappings())
     modImplementation("net.fabricmc:fabric-loader:0.16.9")
 
-    modImplementation(fabricApi.module("fabric-resource-loader-v0", "0.106.1+1.21.2"))
+    modImplementation(fabricApi.module("fabric-resource-loader-v0", "0.109.0+1.21.3"))
 
     modImplementation("com.terraformersmc:modmenu:12.0.0-beta.1")
-    modRuntimeOnly(fabricApi.module("fabric-screen-api-v1", "0.106.1+1.21.2"))
-    modRuntimeOnly(fabricApi.module("fabric-key-binding-api-v1", "0.106.1+1.21.2"))
-    modRuntimeOnly(fabricApi.module("fabric-lifecycle-events-v1", "0.106.1+1.21.2"))
+    modRuntimeOnly(fabricApi.module("fabric-screen-api-v1", "0.109.0+1.21.3"))
+    modRuntimeOnly(fabricApi.module("fabric-key-binding-api-v1", "0.109.0+1.21.3"))
+    modRuntimeOnly(fabricApi.module("fabric-lifecycle-events-v1", "0.109.0+1.21.3"))
 
     modCompileOnly("maven.modrinth:sodium:mc1.21.3-0.6.0-fabric")
 }
