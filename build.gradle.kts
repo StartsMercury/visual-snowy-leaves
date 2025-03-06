354fea8a54e7a7ea72e498ac87b93d346ef3d3a4
StartsMercury <89975834+StartsMercury@users.noreply.github.com>
2025-03-06 10:31:03 +0800

    Update to Minecraft 25w10a

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ object Constants {
     const val VERSION = "0.4.6"
 
     const val VERSION_JAVA = 21
-    const val VERSION_MINECRAFT = "25w09b"
+    const val VERSION_MINECRAFT = "25w10a"
 }
 
 plugins {
@@ -71,7 +71,7 @@ tasks.withType<ProcessResources> {
     val data = mapOf(
         "version" to Constants.VERSION,
         "version_java" to Constants.VERSION_JAVA,
-        "version_minecraft" to "1.21.5-alpha.25.9.b",
+        "version_minecraft" to "1.21.5-alpha.25.10.a",
     )
 
     inputs.properties(data)
