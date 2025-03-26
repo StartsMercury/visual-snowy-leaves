3e4517bf7ef04bcc21019d04ca8f17589a55a795
StartsMercury <89975834+StartsMercury@users.noreply.github.com>
2025-03-26 09:28:19 +0800

    Update Mod Menu to 14.0.0-rc.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -59,7 +59,7 @@ dependencies {
 
     modImplementation(fabricApi.module("fabric-resource-loader-v0", "0.119.5+1.21.5"))
 
-    modImplementation("com.terraformersmc:modmenu:14.0.0-beta.2")
+    modImplementation("com.terraformersmc:modmenu:14.0.0-rc.2")
     modRuntimeOnly(fabricApi.module("fabric-screen-api-v1", "0.119.5+1.21.5"))
     modRuntimeOnly(fabricApi.module("fabric-key-binding-api-v1", "0.119.5+1.21.5"))
     modRuntimeOnly(fabricApi.module("fabric-lifecycle-events-v1", "0.119.5+1.21.5"))
