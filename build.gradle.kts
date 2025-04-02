ef998ae88b0160c0bb95a94144205c5f635925e7
StartsMercury <89975834+StartsMercury@users.noreply.github.com>
2025-04-02 09:31:46 +0800

    Update Fabric Loader to 0.16.11

--- build.gradle.kts
+++ build.gradle.kts
@@ -55,7 +55,7 @@ repositories {
 dependencies {
     minecraft("com.mojang:minecraft:${Constants.VERSION_MINECRAFT}")
     mappings(loom.officialMojangMappings())
-    modImplementation("net.fabricmc:fabric-loader:0.16.10")
+    modImplementation("net.fabricmc:fabric-loader:0.16.11")
 
     modImplementation(fabricApi.module("fabric-resource-loader-v0", "0.119.5+1.21.5"))
 
