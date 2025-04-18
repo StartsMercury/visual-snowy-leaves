6660a49f4e76eb4caab5953c3d17d8480e03ba6a
StartsMercury <89975834+StartsMercury@users.noreply.github.com>
2025-04-18 11:02:51 +0800

    Update snowy texture impl
    Hopefully fixes Pale Oak leaves
    
    (cherry picked from commit bdd21705010875ac1980d73e394daf9ab29debb4)

--- src/client/java/io/github/startsmercury/visual_snowy_leaves/impl/client/SnowableBlockColor.java
+++ src/client/java/io/github/startsmercury/visual_snowy_leaves/impl/client/SnowableBlockColor.java
@@ -3,6 +3,7 @@ package io.github.startsmercury.visual_snowy_leaves.impl.client;
 import io.github.startsmercury.visual_snowy_leaves.impl.client.extension.SnowAware;
 import io.github.startsmercury.visual_snowy_leaves.impl.client.extension.SnowDataAware;
 import io.github.startsmercury.visual_snowy_leaves.impl.client.extension.VisualSnowyLeavesAware;
+import it.unimi.dsi.fastutil.ints.Int2IntMap;
 import net.minecraft.client.color.block.BlockColor;
 import net.minecraft.core.BlockPos;
 import net.minecraft.util.ARGB;
@@ -12,7 +13,7 @@ import org.jetbrains.annotations.Nullable;
 
 import java.util.Objects;
 
-public record SnowableBlockColor(BlockColor blockColor, int correctionMultiplier) implements BlockColor {
+public record SnowableBlockColor(BlockColor blockColor, Int2IntMap correctionMultipliers) implements BlockColor {
     public static final int COLOR_WHITE = 0xFFFFFFFF;
 
     private static boolean isSnowyAt(
@@ -24,16 +25,17 @@ public record SnowableBlockColor(BlockColor blockColor, int correctionMultiplier
             && snowAware.visual_snowy_leaves$coldEnoughToSnow(blockPos);
     }
 
-    public static SnowableBlockColor setMultiplier(BlockColor blockColor, final int multiplier) {
+    public static SnowableBlockColor setMultiplier(BlockColor blockColor, final Int2IntMap multipliers) {
         while (blockColor instanceof final SnowableBlockColor snowableBlockColor) {
             blockColor = snowableBlockColor.blockColor;
         }
 
-        return new SnowableBlockColor(blockColor, multiplier);
+        return new SnowableBlockColor(blockColor, multipliers);
     }
 
     public SnowableBlockColor {
         Objects.requireNonNull(blockColor, "Parameter blockColor is null");
+        Objects.requireNonNull(correctionMultipliers, "Parameter correctionMultipliers is null");
     }
 
     @Override
@@ -43,12 +45,13 @@ public record SnowableBlockColor(BlockColor blockColor, int correctionMultiplier
         final @Nullable BlockPos blockPos,
         final int i
     ) {
-        final var correctionMultiplier = this.correctionMultiplier;
         final var base = this.blockColor.getColor(blockState, blockAndTintGetter, blockPos, i);
+        final var correctionMultiplier = this.correctionMultipliers.get(i);
 
-        if (i != 0) {
+        if (correctionMultiplier == COLOR_WHITE) {
             return base;
         }
+
         if (!(blockAndTintGetter instanceof final VisualSnowyLeavesAware vslAware)) {
             return ARGB.multiply(base, correctionMultiplier);
         }
