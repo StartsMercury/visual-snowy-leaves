19f52987e1976cc094e09c12a8f1f1a412335abf
StartsMercury <89975834+StartsMercury@users.noreply.github.com>
2025-04-26 20:15:51 +0800

    Update to Minecraft 25w17a

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ object Constants {
     const val VERSION = "0.5.0"
 
     const val VERSION_JAVA = 21
-    const val VERSION_MINECRAFT = "25w16a"
+    const val VERSION_MINECRAFT = "25w17a"
 }
 
 plugins {
@@ -71,7 +71,7 @@ tasks.withType<ProcessResources> {
     val data = mapOf(
         "version" to Constants.VERSION,
         "version_java" to Constants.VERSION_JAVA,
-        "version_minecraft" to "1.21.6-alpha.25.16.a",
+        "version_minecraft" to "1.21.6-alpha.25.17.a",
     )
 
     inputs.properties(data)
