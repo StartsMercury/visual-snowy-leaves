3ba6893638317882690261c4bcacf9f03f628069
StartsMercury <89975834+StartsMercury@users.noreply.github.com>
2025-07-02 17:10:35 +0800

    Update to Minecraft 1.21.6 Pre-Release 1

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ object Constants {
     const val VERSION = "0.5.1"
 
     const val VERSION_JAVA = 21
-    const val VERSION_MINECRAFT = "25w21a"
+    const val VERSION_MINECRAFT = "1.21.6-pre1"
 }
 
 plugins {
@@ -73,7 +73,7 @@ tasks.withType<ProcessResources> {
     val data = mapOf(
         "version" to Constants.VERSION,
         "version_java" to Constants.VERSION_JAVA,
-        "version_minecraft" to "1.21.6-alpha.25.21.a",
+        "version_minecraft" to "1.21.6-beta.1",
     )
 
     inputs.properties(data)
