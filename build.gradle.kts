12c19d700fdc1e1ee619404f75c537208937333a
StartsMercury <89975834+StartsMercury@users.noreply.github.com>
2025-09-25 07:30:17 +0800

    Update to Minecraft 1.21.9 Pre-Release 3

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ object Constants {
     const val VERSION = "0.5.2"
 
     const val VERSION_JAVA = 21
-    const val VERSION_MINECRAFT = "1.21.9-pre2"
+    const val VERSION_MINECRAFT = "1.21.9-pre3"
 }
 
 plugins {
@@ -72,7 +72,7 @@ tasks.withType<ProcessResources> {
     val data = mapOf(
         "version" to Constants.VERSION,
         "version_java" to Constants.VERSION_JAVA,
-        "version_minecraft" to "1.21.9-beta.2",
+        "version_minecraft" to "1.21.9-beta.3",
     )
 
     inputs.properties(data)
