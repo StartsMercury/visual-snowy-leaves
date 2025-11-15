f6e8548586cb934fc5152b658c9d808830122c8c
StartsMercury <89975834+StartsMercury@users.noreply.github.com>
2025-11-15 20:53:46 +0800

    Update Fabric Loader to 0.18.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -57,7 +57,7 @@ dependencies {
 
     minecraft("com.mojang:minecraft:${Constants.VERSION_MINECRAFT}")
     mappings(loom.officialMojangMappings())
-    modImplementation("net.fabricmc:fabric-loader:0.17.3")
+    modImplementation("net.fabricmc:fabric-loader:0.18.0")
 
     modImplementation("com.terraformersmc:modmenu:16.0.0-rc.1")
     modRuntimeOnly(fabricApi.module("fabric-lifecycle-events-v1", "0.136.0+1.21.10"))
